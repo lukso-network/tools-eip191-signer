f1febb81be8fa544f0dd739ebc4a9483df26eb05
Hugo Masclet <hugo@lukso.io>
2022-09-20 13:50:29 +0200

    Break prettier

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,32 @@
 /*
  This file contains functions to sign message for executeRelayCall.
 */
-import Account from "eth-lib/lib/account";
-import utils from "web3-utils";
-import { bufferToHex, keccak256 } from "ethereumjs-util";
-import { Message } from "../src/interfaces";
+import Account from 'eth-lib/lib/account';
+import utils from 'web3-utils';
+import { bufferToHex, keccak256 } from 'ethereumjs-util';
+import { Message } from '../src/interfaces';
 
 export class LSP6Signer {
-  hashMessage(message: string) {
+       hashMessage(message: string) {
     const messageHex = utils.isHexStrict(message)
       ? message
-      : utils.utf8ToHex(message);
+      : utils.utf8ToHex(message)
     const messageBytes = utils.hexToBytes(messageHex);
     const messageBuffer = Buffer.from(messageBytes);
-    const preamble = "\x19LSP6 Execute Relay Call:\n" + messageBytes.length;
+    const preamble = '\x19LSP6 Execute Relay Call:\n' + messageBytes.length;
     const preambleBuffer = Buffer.from(preamble);
     const ethMessage = Buffer.concat([preambleBuffer, messageBuffer]);
     return bufferToHex(keccak256(ethMessage));
   }
 
   sign(message: string, privateKey: string): Message {
-    if (!privateKey.startsWith("0x")) {
-      privateKey = "0x" + privateKey;
+    if (!privateKey.startsWith('0x')) {
+      privateKey = '0x' + privateKey;
     }
 
     // 64 hex characters + hex-prefix
     if (privateKey.length !== 66) {
-      throw new Error("Private key must be 32 bytes long");
+      throw new Error('Private key must be 32 bytes long');
     }
 
     const hash = this.hashMessage(message);
@@ -45,15 +45,15 @@ export class LSP6Signer {
   recover(
     message: string | Message,
     signature: string,
-    isMessagePrefixed: boolean
+    isMessagePrefixed: boolean,
   ): string {
     const args = [].slice.apply([message, signature, isMessagePrefixed]);
 
-    if (!!message && typeof message === "object") {
+    if (!!message && typeof message === 'object') {
       return this.recover(
         message.messageHash,
         Account.encodeSignature([message.v, message.r, message.s]),
-        true
+        true,
       );
     }
 
@@ -64,12 +64,12 @@ export class LSP6Signer {
     if (args.length >= 4) {
       isMessagePrefixed = args.slice(-1)[0];
       isMessagePrefixed =
-        typeof isMessagePrefixed === "boolean" ? !!isMessagePrefixed : false;
+        typeof isMessagePrefixed === 'boolean' ? !!isMessagePrefixed : false;
 
       return this.recover(
         message,
         Account.encodeSignature(args.slice(1, 4)),
-        isMessagePrefixed
+        isMessagePrefixed,
       );
     }
     return Account.recover(message, signature);
